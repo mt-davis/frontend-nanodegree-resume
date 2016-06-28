0265a3979314337ffebfb33148e2e5a66d639750
M.T. Davis <marquese.davis@gmail.com>
2016-06-28 18:31:26 +0000

    add: added jobs and education to resume. Also added CSS to biopic

---
+++ js/resumeBuilder-v1.js
@@ -0,0 +1,57 @@
+       var bio = {
+           "name": "Marquese Davis",
+           "role": "Web Developer",
+           "contacts": [{
+               "mobile": "203-589-0000",
+               "email": "marquese.davis@gmail.com",
+               "github": "mt-davis",
+               "twitter": "@mtdavis00",
+               "location": "Connecticut"
+           }],
+           "welcomeMessage": "Welcome to my resume page",
+           "skills": [
+               "programming", "Ruby", "JS"
+           ],
+           "bioPic": "images/fry.jpg"
+       }
+
+       var education = {
+           "schools": [{
+               "name": "Charter Oak State College",
+               "city": "New Britain, CT",
+               "degree": "BA",
+               "major": "Information Technology",
+               "date": "2016",
+               "url": "http://example.com"
+           }],
+           "onlineCourses": [{
+               "title": "JavaScript Course",
+               "school": "Udacity",
+               "date": "2016",
+               "url": "http://udacity.com"
+           }]
+       }
+
+       var work = {
+           "job": [{
+               "employer": "Planet Express",
+               "title": "Delivery Boy",
+               "dates": "Jan 3000",
+               "description": "Who moved my cheese"
+           }]
+       }
+
+       var projects = {
+           "projects": [
+               {
+               "title": "Sameple Project 1",
+               "date": "2014",
+               "description": "Sed vitae congue felis. Donec eu augue id massa sagittis rutrum nec eget quam. Praesent vitae risus et nulla tincidunt congue vel quis massa. Curabitur fermentum libero nec feugiat sodales. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur nec nisl vel nibh placerat rhoncus eget id odio. Vivamus finibus ultricies maximus. Vestibulum sed arcu eget justo congue blandit.",
+               "image": [
+                   "https://example.com"
+               ]
+           }
+         ]
+       }
+       
+       
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,58 +10,10 @@ var bio = {
     },
     "welcomeMessage": "I love coding and helping as many people to learn how to code.",
      "skills" :  ["HTML5","CSS3","JavaScript","Bootstrap","Ruby on Rails", "jQuery","AWS", "Heroku"],
-     "biopic" : "images/fry.jpg"
-};
-var work = {
-    "jobs" : [
-      {
-        "employer": "Bridges... A Community Support",
-        "title": "Director of IT",
-        "dates": "2011-Present",
-        "location": "Milford, CT",
-        "description": "Lead Software developer and IT leader for organzation"
-      }
-    ]
-};
-var education = {
-    "schools" : [
-       {
-        "name" : "Charter Oak State College",
-        "dates" : "2016-Present",
-        "location" : "New Britain, CT",
-        "degree" : "Bachelors Of Information Technology",
-        "majors" : ["Information Technology"]
-       }
-    ],
-   "onlineCourses" : [
-        {
-           "title" : "Front End Nanodegree",
-           "School" : "Udacity",
-           "date" : "Ongoing",
-           "url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-        },
-        {
-           "title" : "Software Product Management",
-           "School" : "Coursera",
-           "date" : "Ongoing",
-           "url" : "https://www.coursera.org/"
-        }
-   ]
+     "biopic" : "images/profile.jpg"
 };
 
-var projects = {
-    "projects" : [
-    {
-         "title" : "Clinical Application",
-         "dates" : "2015",
-         "description" : "This project was to hold the clinical information of clients.",
-         "images" : ["images/inake.jpg"],
-         "link"  : "https://github.com/mt-davis/intakelog"
-    }
-    ]
-};
-
-
+//displying bio information
 bio.display = function()
 {
     var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
@@ -85,7 +37,7 @@ bio.display = function()
     if (bio.skills.length > 0)
     {
        $("#header").append(HTMLskillsStart);
-       for(var i in bio.skills){
+       for (var i in bio.skills){
             var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
             $("#skills").append(formattedSkills);
        }
@@ -93,4 +45,145 @@ bio.display = function()
 };
 
 
+// work 
+var work = {
+    "jobs" : [
+      {
+        "employer": "Bridges... A Community Support",
+        "title": "Director of IT",
+        "dates": "2011-Present",
+        "location": "Milford, CT",
+        "description": "Designs and implements key software solutions for business development, including the private LaaS cloud environment for three collaborating mental health organizations. Developed and deployed custom (IaaS) private cloud environment for EHR at RackSpace.com as Principal Architect. Designed and implemented a web-based clinical intake application using Ruby on Rails to track potential clients/patients on Heroku PaaS."
+      },
+      //second job
+      {
+        "employer":"Community Health Services, Inc",
+        "title": "Director of Management Information Technology",
+        "dates": "October 2004-October 2011",
+        "location": "Hartford, CT",
+        "description": "Led the vision and initiatives of the IT team to add value to the business in line with the organization’s mission statement. Sought and established relationships with external businesses and IT leaders to strategize the most effective IT plans. Supervised the core operations and development functions of IT, including vender relations, department budgeting, and HR."
+      }
+    ]
+};
+
+//displaying employment information
+work.display = function()
+{
+    for(var job in work.jobs) {
+         $("#workExperience").append(HTMLworkStart);
+         var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+         var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+         var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $(".work-entry:last").append(formattedEmployerTitle);
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        $(".work-entry:last").append(formattedLocation);
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+};
+//education
+var education = {
+    "schools" : [
+       {
+        "name" : "Charter Oak State College",
+        "dates" : "2016-Present",
+        "location" : "New Britain, CT",
+        "degree" : "Bachelors Of Information Technology",
+        "majors" : ["Information Technology"]
+       },
+       ],
+     
+    
+   "onlineCourses" : [
+       { "school": "Udacity",
+			"title": "Front-End Web Developer Nanodegree",
+			"completed": "In Progress",
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		},
+		{ "school": "Coursera",
+			"title": "Johns Hopkins University The Data Scientist’s Toolbox",
+			"completed": "March 2015 2014",
+			"url": "https://www.coursera.org/account/accomplishments/verify/CUUPTLWYS3"
+		},
+		{ "school": "Coursera",
+			"title": "Yonsei University Big Data, Cloud Computing, & CDN Emerging Technologies",
+			"completed": "July 2015",
+			"url": "https://www.coursera.org/account/accomplishments/verify/YEKWLFWZ5VNX"
+		},
+		{ "school": "Coursera",
+			"title": "University of Alberta Introduction to Software Product Management",
+			"completed": "January 2016",
+			"url": "https://www.coursera.org/account/accomplishments/verify/J9FZ2LWCGUBS"
+		},	
+		{ "school": "Coursera",
+			"title": "University of Alberta Software Processes and Agile Practices",
+			"completed": "Febuary 2016",
+			"url": "https://www.coursera.org/account/accomplishments/verify/M578VRNL8U32"
+		},
+		{ "school": "edX",
+			"title": "MIT edX Professional Certificate for Startup Success",
+			"completed": "Febuary 2016",
+			"url": "https://courses.edx.org/certificates/a2ba0430dfcf4b37a309a436b5e2382c"
+		}
+	]
+};
+//displaying education infformation
+education.display = function()
+{
+		$('#education').append(HTMLschoolStart);
+		for(school in education.schools){
+			var formattedSchool = HTMLschoolName.replace("%data%", education.schools[school].name);
+			var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+			$('.education-entry:last').append(formattedSchool + formattedDegree);
+
+			var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+			$('.education-entry:last').append(formattedDates);
+
+			var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+			$('.education-entry:last').append(formattedLocation);
+
+			for(major in education.schools[school].majors){
+				var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors[major]);
+				$('.education-entry:last').append(formattedMajor);
+			}
+			if(education.onlineCourses.length > 2) {
+			$("#education").append(HTMLonlineClasses);
+			for(i in education.onlineCourses) {				
+				$("#education").append(HTMLschoolStart);
+				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+				$(".education-entry:last").append(formattedOnlineDates);
+				$(".education-entry:last").append(formattedOnlineURL);
+			}
+			}
+		}
+}; //End of education object
+
+var projects = {
+    "projects" : [
+    {
+         "title" : "Clinical Application",
+         "dates" : "2015",
+         "description" : "This project was to hold the clinical information of clients.",
+         "images" : ["images/inake.jpg"],
+         "link"  : "https://github.com/mt-davis/intakelog"
+    }
+    ]
+};
+
+
+//online.display = function ()
+//{
+// $'education').append.(HTMLonlineClasses);
+//};
+
+
 bio.display();
+education.display();
+work.display();
